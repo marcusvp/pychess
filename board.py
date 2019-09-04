0205c4801bf040e805c6e69dc828f2873a8edf9a
marcusvp <marcus.vp@protonmail.com>
2019-09-04 15:39:59 -0300

    better usage delegation

--- board.py
+++ board.py
@@ -1,12 +1,14 @@
 import pygame
 import init_resources
+
 from player import Player
 from board_interface import BoardInterface
 from move import Move
+from pygame import *
 
 class Board():
 
-    def __init__(self, playerOne, playerTwo, screen):
+    def __init__(self, playerOne, playerTwo):
         self.playerOne = playerOne
         self.playerTwo = playerTwo
         self.pieces_list = []
@@ -14,8 +16,32 @@ class Board():
         self.position_list = []
         self.init_board()
         self.set_pieces()
-        self.interface = BoardInterface(playerOne, playerTwo, self.pieces_list, self.position_list, screen)
+        self.interface = BoardInterface(playerOne, playerTwo, self.pieces_list, self.position_list)
         self.piece_movement = Move(self.position_list)
+        self.turn = "white"
+        
+
+    def start_game(self):
+        self.game_loop()
+
+    def game_loop(self):
+        running = True
+        fps = 60
+        clock = pygame.time.Clock()
+        self.draw_board()
+        #self.flip_board()
+        pygame.display.flip()
+        while running:
+            clock.tick(fps)
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    x, y = event.pos
+                    print("clicked " + str(x), str(y))
+                    self.board_handling(x, y)
+                    self.draw_board()
+                    pygame.display.flip()
+                elif event.type == KEYDOWN and event.key == K_ESCAPE:
+                    running = False
 
     def draw_board(self):
         self.interface.draw()
@@ -30,10 +56,25 @@ class Board():
                 if piece.X() == position.X() and piece.Y() == position.Y():
                     position.set_piece(piece)
     
-    def mouse_handling(self, mouseX, mouseY):
-        if mouseX < 640 and mouseY < 640:
-            self.piece_movement.move_piece(mouseX, mouseY)
+    def remove_pieces(self):
+        for position in self.position_list:
+            position.remove_piece()
+    
+    def board_handling(self, mouse_x, mouse_y):
+        if mouse_x < 640 and mouse_y < 640:
+            if self.piece_movement.move_piece(mouse_x, mouse_y):
+                self.change_turn()
+    
+    def change_turn(self):
+        if self.turn == "white":
+            self.turn = "black"
+        else:
+            self.turn = "white"
 
-            
-        
-                        
+    def flip_board(self):
+        self.remove_pieces()
+        for position in self.position_list:
+            position.flip()
+        for piece in self.pieces_list:
+            piece.flip()
+        self.set_pieces()
--- board_interface.py
+++ board_interface.py
@@ -3,24 +3,25 @@ import init_resources
 
 class BoardInterface():
 
-    def __init__(self, playerOne, playerTwo, pieces_list, tiles_list, screen):
+    def __init__(self, playerOne, playerTwo, pieces_list, tiles_list):
         self.playerOne = playerOne
         self.playerTwo = playerTwo
         self.pieces_list = pieces_list
         self.tiles_list = tiles_list
-        self.screen = screen
+        pygame.init()
+        width, height = 640, 640
+        self.screen = pygame.display.set_mode((width, height))
 
     def draw(self):
         self.draw_tiles()
         self.draw_pieces()
 
-    def draw_pieces(self):
-        for position in self.tiles_list:
-            if position.has_piece():
-                pieceimage = pygame.image.load(str(position.pos_piece().pieceImage()))
-                self.screen.blit(pieceimage, (position.pos_piece().drawPosition()))
-    
     def draw_tiles(self):
         for tile in self.tiles_list:
             tileImage = pygame.image.load(str(tile.drawImage()))
             self.screen.blit(tileImage, (tile.tile_position()))
+            
+    def draw_pieces(self):
+        for piece in self.pieces_list:
+            piece_image = pygame.image.load(str(piece.pieceImage()))
+            self.screen.blit(piece_image, (piece.drawPosition()))
